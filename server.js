d50fdb51cfaf226e69d0f15c28e1854a78403ab6
Parth <parth@prkat.in>
2021-02-08 02:10:00 +0530

    Commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 const app = express()
 
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use(express.static(__dirname + '/public'))
 
 // this * route is to serve project on different page routes except root `/`
 const port = process.env.PORT || 8080
