e8edfb2b25ca6fb82cd2864ef6b38d32201584c2
Parth <parth@prkat.in>
2021-02-07 16:40:07 +0530

    Adding files.

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const path = require('path')
 const app = express()
 
 //here we are configuring dist to serve app files
-app.use(express.static(path.join(__dirname, "client", "dist")))
+app.use(express.static(path.join(__dirname, 'dist/')));
+
+
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
